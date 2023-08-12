558dd10791873485f357f94eee6bc10c8c7fea1d
JR59588 <jvishwajith.rathod@zensar.com>
2023-08-12 13:17:35 +0530

    updated scripts :

--- hlf-cards/duplicateorg.sh
+++ hlf-cards/duplicateorg.sh
@@ -52,27 +52,9 @@ cp -r $1-scripts $2-scripts
 cd $2-scripts
 
 find . -type f -exec sed -i "s|$1|$2|g" {} +
-
+#sed -i "s|$12|$11|g" joinChannel.sh 
 echo "the above changed code is working,,,.......56 line"
-# Function to add text before a specific text in the same file
-# add_text_before() {
-#     local target_text="$1"
-#     local text_to_add="$2"
-#     local input_file="$3"
-# echo "the above changed code is working,,,.......62 line"
-#     # Escape special characters for use in sed
-#     local escaped_target_text=$(sed "s|[\/&]|\\&|g" <<< "$target_text")
-#     echo "the above changed code is working,,,.......64 line"
-#     local escaped_text_to_add=$(sed "s|[\/&]|\\&|g" <<< "$text_to_add")
-# echo "the above changed code is working,,,.......66 line"
-#     # Use sed to replace the target text with the added text and the target text
-#     sed "s|$escaped_target_text|$escaped_text_to_add\\
-# $escaped_target_text|" "$input_file" > "${input_file}.tmp"
-#     echo "the above changed code is working,,,.......69 line"
-#     mv "${input_file}.tmp" "$input_file"
 
-#     echo "Text added successfully."
-# }
 echo $PWD
 add_text_before() {
     local target_text="$1"
@@ -94,24 +76,32 @@ $escaped_target_text|" "$input_file" > "${input_file}.tmp"
 echo "the above changed code is working,,,.......line 73"
 
 
-
 # Usage
 cd ./..
 echo $PWD
-add_text_before "###token" "export PEER0_$2_CA=\${PWD}/organizations/peerOrganizations/$2.hlfdec.com/peers/peer0.$2.hlfdec.com/tls/ca.crt" envVar.sh
+add_text_before "###token1" "export PEER0_$2_CA=\${PWD}/organizations/peerOrganizations/$2.hlfdec.com/peers/peer0.$2.hlfdec.com/tls/ca.crt" envVar.sh
+echo >> envVar.sh
 
-add_text_before "###happen" "  elif [ \$USING_ORG -eq 7 ]; then" envVar.sh
-add_text_before "###happen" "    echo \"setting peer env for \$USING_ORG\"" envVar.sh
-add_text_before "###happen" "    export CORE_PEER_LOCALMSPID=\"$2MSP\"" envVar.sh
-add_text_before "###happen" "    export CORE_PEER_TLS_ROOTCERT_FILE=\$PEER0_$2_CA" envVar.sh
-add_text_before "###happen" "    export CORE_PEER_MSPCONFIGPATH=\${PWD}/organizations/peerOrganizations/$2.hlfdec.com/users/Admin@$2.hlfdec.com/msp" envVar.sh
-add_text_before "###happen" "    export CORE_PEER_ADDRESS=localhost:$7" envVar.sh
-   
-add_text_before "###bumble" "  elif [ \$USING_ORG -eq 7 ]; then" envVar.sh
-add_text_before "###bumble" "    export CORE_PEER_ADDRESS=peer0.$2.hlfdec.com:$7" envVar.sh
+add_text_before "###token2" "  elif [ \$USING_ORG -eq $11 ]; then" envVar.sh
+add_text_before "###token2" "    echo \"setting peer env for \$USING_ORG\"" envVar.sh
+add_text_before "###token2" "    export CORE_PEER_LOCALMSPID=\"$2MSP\"" envVar.sh
+add_text_before "###token2" "    export CORE_PEER_TLS_ROOTCERT_FILE=\$PEER0_$2_CA" envVar.sh
+add_text_before "###token2" "    export CORE_PEER_MSPCONFIGPATH=\${PWD}/organizations/peerOrganizations/$2.hlfdec.com/users/Admin@$2.hlfdec.com/msp" envVar.sh
+add_text_before "###token2" "    export CORE_PEER_ADDRESS=localhost:$7" envVar.sh
+echo >> envVar.sh
 
-add_text_before "###tinder" "  elif [ \$ORG -eq 7 ]; then" setAnchorPeer.sh
-add_text_before "###tinder" "    HOST=\"peer0.$2.hlfdec.com\"" setAnchorPeer.sh
-add_text_before "###tinder" "    PORT=$7" setAnchorPeer.sh
+add_text_before "###token3" "  elif [ \$USING_ORG -eq $11 ]; then" envVar.sh
+add_text_before "###token3" "    export CORE_PEER_ADDRESS=peer0.$2.hlfdec.com:$7" envVar.sh
+echo >> envVar.sh
+
+add_text_before "###token4" "  elif [ \$ORG -eq $11 ]; then" setAnchorPeer.sh
+add_text_before "###token4" "    HOST=\"peer0.$2.hlfdec.com\"" setAnchorPeer.sh
+add_text_before "###token4" "    PORT=$7" setAnchorPeer.sh
+echo >> setAnchorPeer.sh
 
 echo "the above changed code is working,,,.......line 92 "
+
+cd ./..
+echo $PWD
+#./$2.sh up createChannel -c channel1 -ca -s chouchdb
+
